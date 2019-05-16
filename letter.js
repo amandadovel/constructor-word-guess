d51d08e7c3c33f643213616a7565c3d21085df42
Amanda Dovel <amandacdovel@gmail.com>
2019-05-16 12:20:34 -0600

    add gitignore file for node modules

---
+++ letter.js
@@ -0,0 +1,22 @@
+// dependency for inquirer npm package
+var inquirer = require("inquirer");
+
+// global variables
+var input = process.argv[2];
+
+
+
+// Contains constructor, letter containing character or placeholder  
+// depending on if letter has been guessed
+// function letter(string) {
+//     this.string = string;
+//     this.guessed = false;
+//     this.renderLetter = function () {
+//         for (var i = 0; i > string.length; i++);
+//         let letter = string.length[i];
+//         console.log(letter);
+//     }
+//     this.checkLetter = function () {
+
+//     }
+// }
